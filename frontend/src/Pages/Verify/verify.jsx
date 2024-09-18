c1252073faba685bbdb4df1fb86ce17383725b85
daposarumi <sarumiadedapo@gmail.com>
2024-09-18 22:45:58 +0100

    new

--- frontend/src/Pages/Verify/verify.jsx
+++ frontend/src/Pages/Verify/verify.jsx
@@ -4,34 +4,32 @@ import { useNavigate, useSearchParams } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext';
 import axios from 'axios'
 
-
 export const Verify = () => {
 
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const success = searchParams.get("success")
     const orderId = searchParams.get("orderId")
 
-    const {url} = useContext(ShopContext)
+    const { url } = useContext(ShopContext)
     const navigate = useNavigate();
 
-    const verifyPayment = async () => {
-        const response = await axios.post(url+"/api/order/verify",{success,orderId})
-        if (response.data.success) {
-            navigate("/myorders")
+    useEffect(() => {
+        const verifyPayment = async () => {
+            const response = await axios.post(url + "/api/order/verify", { success, orderId })
+            if (response.data.success) {
+                navigate("/myorders")
+            } else {
+                navigate("/")
+            }
         }
-        else{
-            navigate("/")
-        }
-    }
 
-    useEffect(()=>{
         verifyPayment();
-    },[])
+    }, [success, orderId, url, navigate]) // Added dependencies
 
-    console.log(success,orderId);
-  return (
-    <div className='verify'>
-        <div className='spinner'></div>
-    </div>
-  )
+    console.log(success, orderId);
+    return (
+        <div className='verify'>
+            <div className='spinner'></div>
+        </div>
+    )
 }
