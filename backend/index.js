bc0cc9c74493cde51bd1ade31c61674054199589
daposarumi <sarumiadedapo@gmail.com>
2024-10-01 20:02:21 +0100

    new

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,8 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200,
+    credentials: true
 };
 
 app.use(cors(corsOptions));
